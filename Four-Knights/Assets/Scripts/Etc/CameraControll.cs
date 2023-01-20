18f42b627170b674dd26aa2f4567c9903fe2e02b
JiUnKang <46744270+rkdwldns00@users.noreply.github.com>
2023-01-20 18:49:12 +0900

    camera tracking script

---
+++ Four-Knights/Assets/Scripts/Etc/CameraControll.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    GameObject cam;
+    float camRotX = 0;
+    float camRotY = -50;
+    float camDistance = 3;
+    float dps = 4;
+
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            camRotY = Mathf.Clamp(camRotY +Input.GetAxis("Mouse Y") * dps,-40,0);
+            camRotX += Input.GetAxis("Mouse X") * dps;
+            camDistance = Mathf.Clamp(camDistance + Input.GetAxis("Mouse ScrollWheel"),1,3);
+        }
+
+        Vector3 headPos = transform.position + Vector3.up * 1.5f;
+        RaycastHit hit;
+        if (Physics.Raycast(headPos, Quaternion.Euler(camRotY, camRotX, 0) * Vector3.forward, out hit, camDistance))
+        {
+            cam.transform.position = hit.point;
+        }
+        else
+        {
+            cam.transform.position = headPos + Quaternion.Euler(camRotY, camRotX, 0) * Vector3.forward * camDistance;
+        }
+        cam.transform.LookAt(headPos);
+    }
+}
